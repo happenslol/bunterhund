89e3fdf98c5d7006ae888536df9744ec97217ff2
Hilmar Wiegand <hilmar.wiegand@flameshome.net>
2018-04-14 18:40:25 +0200

    :tada: Initial commit

---
+++ src/main/kotlin/com/happens/bunterhund/BunterhundApplication.kt
@@ -0,0 +1,11 @@
+package com.happens.bunterhund
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BunterhundApplication
+
+fun main(args: Array<String>) {
+    runApplication<BunterhundApplication>(*args)
+}
---
+++ src/test/kotlin/com/happens/bunterhund/BunterhundApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.happens.bunterhund
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BunterhundApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
